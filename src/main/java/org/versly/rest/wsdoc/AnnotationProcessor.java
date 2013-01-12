2b46df55f7183d5dfb5748b2b4026b149732f77d
ryanwalls <github@ryanwalls.com>
2013-01-11 19:01:03 -0700

    Add support for domain objects that are in a hierarchy where some parents are generic types.

--- src/main/java/org/versly/rest/wsdoc/AnnotationProcessor.java
+++ src/main/java/org/versly/rest/wsdoc/AnnotationProcessor.java
@@ -16,6 +16,7 @@
 
 package org.versly.rest.wsdoc;
 
+import com.sun.tools.javac.code.Type;
 import org.codehaus.jackson.annotate.JsonIgnore;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -424,7 +425,8 @@ public class AnnotationProcessor extends AbstractProcessor {
 
             // replace variables with the current concrete manifestation
             if (type instanceof TypeVariable) {
-                type = getDeclaredTypeForTypeVariable((TypeVariable) type);
+                Type.MethodType methodType = (Type.MethodType) processingEnv.getTypeUtils().asMemberOf(_type, executableElement);
+                type = methodType.getReturnType();
                 if (type == null)
                     return; // couldn't find a replacement -- must be a generics-capable type with no generics info
             }
---
+++ src/test/resources/org/versly/rest/wsdoc/ChildController.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2011 TaskDock, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.versly.rest.wsdoc;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+public class ChildController {
+
+    /**
+     * Retrieves the child
+     */
+    @RequestMapping(value = "/child", method = RequestMethod.GET)
+    public Child getChild() {
+        return null;
+    }
+
+}
